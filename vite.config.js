8ad2b956fda513c6f98655e275417f3c3cf62539
Mikek1977 <mkrucejr@gmail.com>
2024-07-29 01:06:56 -0400

    updated json

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/React-Portfolio/' // This should match your GitHub repository name
 });
